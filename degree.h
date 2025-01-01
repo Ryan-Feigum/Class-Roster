f48347a89888f884349cd24e8ffa5387c875f890
Ryan Feigum <23489211+Ryan-Feigum@users.noreply.github.com>
2025-01-01 17:49:23 -0600

    Initial commit

---
+++ degree.h
@@ -0,0 +1,3 @@
+/*
+Define an enumerated data type DegreeProgram for the degree programs containing the data type values SECURITY, NETWORK, and SOFTWARE.
+*/
---
+++ student.h
@@ -0,0 +1,29 @@
+/*  Create the class Student  in the files student.h and student.cpp, which includes each of the following variables:
+
+•  student ID
+
+•  first name
+
+•   last name
+
+•  email address
+
+•  age
+
+•  array of number of days to complete each course
+
+•  degree program
+
+2.  Create each of the following functions in the Student class:
+
+a.  an accessor (i.e., getter) for each instance variable from part D1
+
+b.  a mutator (i.e., setter) for each instance variable from part D1
+
+c.  All external access and changes to any instance variables of the Student class must be done using accessor and mutator functions.
+
+d.  constructor using all of the input parameters provided in the table
+
+e.  print() to print specific student data
+
+*/
